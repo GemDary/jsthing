b91d0a577ca976dfac9e3ea7d4a85a6f662c46a7
Hendri <hendri.nommik@khk.ee>
2022-09-20 09:43:51 +0100

    big moment

--- js_basics.js
+++ js_basics.js
@@ -2,7 +2,19 @@
 let name = "Hendri Nõmmik"
  name = "New name"
 
- // const
+ let goodmark = 5
+ console.log(goodmark)
+ console.log(typeof goodmark)
+
+ let persent = 50.3
+ console.log(persent)
+ console.log(typeof persent)
+
+ let ishere = true // false
+ console.log(ishere)
+ console.log(typeof ishere)
+
+/* // const
  const surname = "Nõmmik"
 
  // arrays
@@ -19,4 +31,39 @@ let name = "Hendri Nõmmik"
  console.log(person.firstname)
  console.log(person.surname)
  person.department = "IKT"
- console.log(person)
+ console.log(person)*/
+
+ // null
+ const airplane = null
+ console.log(airplane)
+ console.log(typeof airplane)
+
+ // Symbol
+ const symbol = Symbol()
+ console.log(symbol)
+ console.log(typeof symbol)
+
+ // undefined
+ let test
+ console.log(test)
+ console.log(typeof test)
+ // Number -> String
+ let val
+ console.log(555)
+ val = String(555)
+ val = String(4+4)
+ val = String(5.8)
+ // Boolean -> String
+ val = String(true)
+ val = String(false)
+ // Object -> String
+ val = String(new Date())
+ // Null -> String
+ val = String(null)
+// Array -> String
+ val = String([1, 2, 3, 4, 5])
+ // Undefined -> String
+ val = String(undefined)
+ // toString()
+ val = (true).toString()
+ console.log(val)
