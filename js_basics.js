8748d6a36ef7849421a4840fe4440c23cb34f155
Hendri <hendri.nommik@khk.ee>
2022-09-20 09:19:38 +0100

    big moment

---
+++ js_basics.js
@@ -0,0 +1,22 @@
+ // variables
+let name = "Hendri Nõmmik"
+ name = "New name"
+
+ // const
+ const surname = "Nõmmik"
+
+ // arrays
+ const numbers = [1, 2, 3, 4, 5]
+ console.log(numbers)
+ numbers.push(6)
+ console.log(numbers)
+ // objects
+ const person = {
+    firstname: "Hendri",
+     surname: "Nõmmik"
+ }
+ console.log(person)
+ console.log(person.firstname)
+ console.log(person.surname)
+ person.department = "IKT"
+ console.log(person)
