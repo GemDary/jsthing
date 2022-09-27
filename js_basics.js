3a38f6d1a480bdd5dcf8bb9988e259ca4d993edf
Hendri <hendri.nommik@khk.ee>
2022-09-27 07:54:34 +0100

    template

--- js_basics.js
+++ js_basics.js
@@ -1,34 +1,16 @@
-let val;
+const li = document.createElement("li")
+li.className = "collection-item"
+const text = document.createTextNode("Study element creation")
+li.appendChild(text)
+
+const a = document.createElement("a")
+a.className = "blue-text text-darken 2 secondary-content"
+text = document.createTextNode("X")
+a.appendChild(text)
+a.setAttribute("href", "#")
+
+li.appendChild(a)
 
 const list = document.querySelector("ul")
-const listitem = document.querySelector("Li")
-
-console.log(listitem)
-
-val = list
-val = list.childNodes
-val = list.childNodes[1]
-val = list.childNodes[1].nodeName
-val = list.childNodes[1].nodeType
-
-val = list.children
-val = list.children[1].children
-
-val = list.firstChild
-val = list.firstElementChild
-
-val = list.lastChild
-val = list.lastElementChild
-
-val = list.childElementCount
-
-val = listitem.parentNode
-val = listitem.parentElement
-val = listitem.parentElement.parentElement
-
-val = listitem.nextSibling
-val = listitem.nextElementSibling
-val = listitem.nextElementSibling
-val = listitem.nextElementSibling.nextElementSibling.previousElementSibling
-
-console.log(val)
+list.appendChild(li)
+console.log(a)
