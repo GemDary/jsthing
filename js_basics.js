a8f5f42ebf3f6140fcdf5ed254b091369eaaf875
GemDary <94610697+GemDary@users.noreply.github.com>
2022-09-29 08:45:10 +0100

    Update js_basics.js

--- js_basics.js
+++ js_basics.js
@@ -1,16 +1,21 @@
-const li = document.createElement("li")
-li.className = "collection-item"
-const text = document.createTextNode("Study element creation")
-li.appendChild(text)
+const form  = document.querySelector('form')
+const taskInput = document.querySelector('#task')
 
-const a = document.createElement("a")
-a.className = "blue-text text-darken 2 secondary-content"
-text = document.createTextNode("X")
-a.appendChild(text)
-a.setAttribute("href", "#")
+form.addEventListener('submit', addTask)
 
-li.appendChild(a)
-
-const list = document.querySelector("ul")
-list.appendChild(li)
-console.log(a)
+function addTask(e){
+    // create list item
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(taskInput.value))
+    li.className = 'collection-item'
+    const a = document.createElement('a')
+    a.appendChild(document.createTextNode('X'))
+    a.className = 'blue-text text-darken-2 secondary-content'
+    a.setAttribute('href', '#')
+    li.appendChild(a)
+    // add to list adsada
+    const ul = document.querySelector('ul')
+    ul.appendChild(li)
+    taskInput.value = ''
+    e.preventDefault()
+}
