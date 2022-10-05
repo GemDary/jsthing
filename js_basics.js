d594323bbad603ea4ba801b98f979434303b0ac3
GemDary <94610697+GemDary@users.noreply.github.com>
2022-10-05 08:36:02 +0100

    Update js_basics.js

--- js_basics.js
+++ js_basics.js
@@ -1,14 +1,12 @@
 const form  = document.querySelector('form')
-
 const taskInput = document.querySelector('#task')
-const taskList = document.querySelector("ul")
-const deletAllTasks = document.querySelector("#del-tasks")
-
-deletAllTasks.addEventListener("click", deleteAllTasks)
-taskList.addEventListener("click", deleteTask)
-
+const taskList = document.querySelector('ul')
+const delAllTasks = document.querySelector('#del-tasks')
 
 form.addEventListener('submit', addTask)
+taskList.addEventListener('click', deleteTask)
+delAllTasks.addEventListener('click', deleteAllTasks)
+document.addEventListener('DOMContentLoaded', getTasks)
 
 function addTask(e){
     // create list item
@@ -20,17 +18,19 @@ function addTask(e){
     a.className = 'blue-text text-darken-2 secondary-content'
     a.setAttribute('href', '#')
     li.appendChild(a)
-    // add to list adsada
+    // add to list
     const ul = document.querySelector('ul')
     ul.appendChild(li)
+    addTaskLS(taskInput.value)
     taskInput.value = ''
     e.preventDefault()
 }
 
 function deleteTask(e){
-    if (e.target.textContent == "X") {
-        if(confirm("Are you sure you want to delete this task?")) {
+    if(e.target.textContent == 'X'){
+        if(confirm('Are you sure to delete this task?')){
             e.target.parentElement.remove()
+            deleteTaskLS(e.target.parentElement.textContent.slice(0,-1))
         }
     }
 }
@@ -40,4 +40,60 @@ function deleteAllTasks(e){
     while (taskList.firstChild){
         taskList.removeChild(taskList.firstChild)
     }
+    localStorage.removeItem('tasks')
 }
+
+function addTaskLS(task) {
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function deleteTaskLS(task) {
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((taskLS, taskIndex) => {
+        if(taskLS === task){
+            tasks.splice(taskIndex, 1)
+        }
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function getTasks(){
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((task) => {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(task))
+        li.className = 'collection-item'
+        const a = document.createElement('a')
+        a.appendChild(document.createTextNode('X'))
+        a.className = 'blue-text text-darken-2 secondary-content'
+        a.setAttribute('href', '#')
+        li.appendChild(a)
+        // add to list
+        const ul = document.querySelector('ul')
+        ul.appendChild(li)
+    })
+}
+Footer
+© 2022 GitHub, Inc.
+    Footer navigation
+Terms
+Privacy
+Security
+Status
